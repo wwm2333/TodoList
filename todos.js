0c2ab93e7a00779ed5a9994fdd84491ee51e95c9
wwm2333 <229483126u@qq.com>
2019-05-04 00:26:25 +0800

    js-end

--- todos.js
+++ todos.js
@@ -17,31 +17,64 @@ class TodoList{
         Utils.delete(id);
         return this.render();
     }
-    edit(id,text,completed){
-        let todo=new Todo(id,text,completed);
+    completed(id,completed){
+        let todo=Utils.save(id);
+        todo.completed=completed;
         Utils.save(id,todo);
         return this.render();
     }
-    _hasCompeleted(){
-        var complateds=this._filter('completed');
-        return (complateds!=null&&complateds.length>0);
+    edit(id,text){
+        let todo=Utils.save(id);
+        if(text){
+            let title=text.replace(/^\s+|\s+$/g,"");
+            if(title){
+                todo.text=title;
+                Utils.save(id,todo);
+            }
+        }
+        return this.render();
+    }
+    checkedAll(completed){
+        let todos=Utils.getAll()||[];
+        todos.forEach(todo=>{
+            todo.completed=completed;
+            Utils.save(todo.id,todo);
+        });
+        return this.render();
+    }
+    clearCompleted(){
+        var completeds=this._filter('completed');
+        if(completeds.length>0){
+            completeds.forEach(todo=>{
+                Utils.delete(todo.id);
+            });
+        }
+        return this.render();
+    }
+    clear(){
+        Utils.clear();
+        return this.render();
+    }
+    _hasCompleted(){
+        var completeds=this._filter('completed');
+        return (completeds!=null&&completeds.length>0);
     }
     _filter(flag=''){
         const todos= Utils.getAll()||[];
         if(flag=='active'){
-          return todos.filter(todo=>!todo.complated);
+          return todos.filter(todo=>!todo.completed);
         }
-        if(flag=='complated'){
-            return todos.filter(todo=>todo.complated);
+        if(flag=='completed'){
+            return todos.filter(todo=>todo.completed);
         }
         return todos;
     }
     filter(flag=''){
-        this.render(this._filter(flag));
+        return this.render(this._filter(flag),flag);
     }
-    render(todolist){
+    render(todolist,flag=''){
        let listhtml=this._renderList(todolist);
-       let footerhtml=this._renderfooter();
+       let footerhtml=this._renderfooter(flag);
         return {
             listhtml:listhtml,
             footerhtml:footerhtml
@@ -52,37 +85,40 @@ class TodoList{
         let todos=todolist||Utils.getAll()||[];
             todos.forEach(todo => {
                 html+=`
-                <div class="view">
-                    <input class="toggle" type="checkbox" ${todo.completed?'checked':''}>
-                    <label>${todo.text}</label>
-                    <button class="destroy" data-id='${todo.id}'></button>
-                </div>
-                <input class="edit" value="${todo.text}"></input>
-                    `
+                <li class='${todo.completed?'completed':''}'>
+                    <div class="view">
+                        <input class="toggle" data-id='${todo.id}' type="checkbox" ${todo.completed?'checked':''}>
+                        <label>${todo.text}</label>
+                        <button class="destroy" data-id='${todo.id}'></button>
+                    </div>
+                <input class="edit" value="${todo.text}" data-id='${todo.id}'></input>
+                </li> 
+                `
             }); 
         return html;
      }
-     _renderfooter(){
+     _renderfooter(flag=''){
         let remaining=this._filter('active').length;
-        if(!remaining){
+        let hascompleted=this._hasCompleted();
+        if(!remaining&&!hascompleted){
             return '';
         }
          let html= `
-         <span class="todo-count"><strong>${remaining}${remaining === 1 ? 'item' : 'items'} left</span>
+         <span class="todo-count"><strong>${remaining}${remaining > 1 ? 'items' : 'item'} left</span>
             <ul class="filters">
                 <li>
-                    <a class="selected" href="#/">All</a>
+                    <a class="${flag==''?'selected':''}" href="#/">All</a>
                 </li>
                 <li>
     
-                    <a href="#/active">Active</a>
+                    <a href="#/active" class="${flag=='active'?'selected':''}">Active</a>
                 </li>
                 <li>
-                    <a href="#/completed">Completed</a>
+                    <a href="#/completed" class="${flag=='completed'?'selected':''}">Completed</a>
                 </li>
             </ul>
          `;
-        if(this._hasCompeleted){
+        if(hascompleted){
            html+= `<button class="clear-completed">Clear completed</button>`;              
         }
         return html;
@@ -91,7 +127,7 @@ class TodoList{
 
 const Utils = {
     
-      save(key, data) {
+    save(key, data) {
         if (arguments.length > 1) {
          return localStorage.setItem(key, JSON.stringify(data));
       } else {
@@ -99,24 +135,29 @@ const Utils = {
         return (storeData && JSON.parse(storeData)) || []; 
         }
      },
-     delete(key){
+    delete(key){
         if(localStorage.hasOwnProperty(key)){
             localStorage.removeItem(key);
         } 
      },
-     getAll(){
+    getAll(){
          let allData=[];
          for (let index = 0; index < localStorage.length; index++) {
             allData.push(JSON.parse(localStorage.getItem(localStorage.key(index))));
          }
-       return allData||[];
+         let datas=allData.sort((a,b)=>{
+             return b.id-a.id
+         });
+       return datas||[];
      },
-     getOrder(){ 
-         var ids=[];
+    getOrder(){ 
+        var ids=[];
         this.getAll().forEach(data=>{
             ids.push(data.id);
         });
-         return Math.max(...ids)+1||1;
+         return ids.length>0?Math.max(...ids)+1:1;
      },
-   
+    clear(){
+       localStorage.clear();
+   }
 }
