f0cc90ab61cfd07c5bdd80b22fd799e1d302dc42
wwm2333 <229483126@qq.com>
2019-06-27 17:42:53 +0800

    checkedAll

--- app/component/app.js
+++ app/component/app.js
@@ -11,26 +11,33 @@ class App extends React.Component {
         data: this.props.data
       }
       
-     // this.handleChoseAll=this.handleChoseAll.bind(this);
+     this.handleCheckedAll=this.handleCheckedAll.bind(this);
       this.handleAddTodo=this.handleAddTodo.bind(this);
   }
-/*handleChoseAll(){
+    handleCheckedAll(checked){
+        /*this.setState({
+            choseAll:!this.state.choseAll
+        });*/
+        let todos=this.state.data;
+        todos.array.forEach(todo => {
+            todo.completed=checked;
+        });
+        this.setState({
+            data:todos
+        });
+    }
+    handleAddTodo(newTodo){
     this.setState({
-        choseAll:!this.state.choseAll
-    });
-}*/
-handleAddTodo(newTodo){
-   this.setState({
-       date:[...this.state.data,newTodo]
-   })
-}
+        date:[...this.state.data,newTodo]
+    })
+    }
 
   render () {
     const { data } = this.state; 
     return (
         <div>
 			<AppForm handleAddTodo={this.handleAddTodo} ></AppForm>
-            <AppList data={data}></AppList>
+            <AppList data={data} checkedAll={this.handleCheckedAll}></AppList>
             <AppFooter></AppFooter>
         </div>
     )
--- main.js
+++ main.js
@@ -4,9 +4,9 @@ import ReactDOM from 'react-dom'
 import App from './app/component/app'
 
 let data = [
- {id: 0, text: '天气不错哦!!!', complete: false},
- {id: 1, text: '天气不错哦!!!', complete: false},
- {id: 2, text: '出去玩啊!!!', complete: true},
+ {id: 0, text: '天气不错哦!!!', completed: false},
+ {id: 1, text: '天气不错哦!!!', completed: false},
+ {id: 2, text: '出去玩啊!!!', completed: false},
 ]
 
 ReactDOM.render(
